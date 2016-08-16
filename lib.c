5ee24e79b16cd71af6afbf3ef8dde711c07b0217
lysu <sulifx@gmail.com>
2016-08-17 02:39:28 +0800

    test

---
+++ lib.c
@@ -0,0 +1,10 @@
+//
+// Created by suli on 8/14/16.
+//
+
+#include "lib.h"
+#include <stdio.h>
+
+void test() {
+    printf("1111");
+}
---
+++ lib.h
@@ -0,0 +1,11 @@
+//
+// Created by suli on 8/14/16.
+//
+
+#ifndef C_MISC_LIB_H
+#define C_MISC_LIB_H
+
+inline void test();
+
+#endif //C_MISC_LIB_H
+

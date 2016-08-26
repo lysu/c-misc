1b62470aa6e2658212d97350c205e037619d3cc4
lysu <sulifx@gmail.com>
2016-08-26 23:13:25 +0800

    make test

--- test2/main.c
+++ test2/main.c
@@ -1,4 +1,18 @@
-//
-// Created by suli on 8/25/16.
-//
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+void toUpper(char *str) {
+    while (*str) {
+        *str = (char)toupper((int)*str);
+        str++;
+    }
+}
+
+int main(void) {
+    char a[] = "abc";
+    toUpper(a);
+    fputs(a, stdout);
+    exit(EXIT_SUCCESS);
+}
 
--- test3/erf.c
+++
@@ -1,4 +0,0 @@
-#include <math.h>
-#include <stdio.h>
-
-int main(void) { printf(".... is %g\n", erf(1.96 * sqrt(1 / 2.))); }

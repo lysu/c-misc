cb65ca901de05c6814e8efea03acb0624ecb332d
lysu <sulifx@gmail.com>
2020-01-11 13:29:14 +0800

    test madvise

--- inline/main.c
+++ inline/main.c
@@ -1,12 +1,23 @@
-#include <stdio.h>
-#include "target.h"
+#include<stdio.h>
+#include<sys/mman.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
 
-int main(void) {
-# if __GNUC__ && !__GNUC_STDC_INLINE__
-    printf("gnuc----\n");
-# else
-    printf("not gnuc----\n");
-# endif
-    int x = plus(1, 2);
-    printf("%x\n", x);
+#define SIZE  1023*1024*1024
+
+int main(int argc, char *argv[]) {
+    char *p;
+    if ((p = (char *) mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0)) ==
+        (void *) -1) {
+        perror("mmap");
+    }
+    memset(p, 'c', SIZE);
+    int r = madvise(p, SIZE, MADV_FREE);
+    if (r < 0) {
+        perror("madvise");
+    }
+    printf("Get 1 GB...\n");
+    sleep(3000);
+    return 0;
 }
